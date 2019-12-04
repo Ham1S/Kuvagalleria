6fc9ea544d56bd0f05ba25277e111fede84b2d52
Ham1S <47852325+Ham1S@users.noreply.github.com>
2019-12-04 12:59:59 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    <link rel="stylesheet" type="style/css" href="bootstrap.min.css">
 
 </head>
 <body>
@@ -19,7 +19,11 @@
         <div class="header-login">
             <?php
             if (isset($_SESSION['userId'])) {
-                echo '<form action="login/logout.inc.php" method="post">
+                echo '<form action="upage.php" method="post">
+                <button type="submit">Upload images</button>
+                </form>
+                
+                <form action="login/logout.inc.php" method="post">
                 <button type="submit" name="logout-submit">Logout</button>
                 <p class="login-status">You are logged in</p>
                 </form>';
@@ -27,6 +31,7 @@
                 echo '<form action="login/login.inc.php" method="post">
                 <button type="submit" name="login-submit">Login</button>
                 </form>
+
                 <form action="login/signup.inc.php" method="post">
                 <button type="submit" name="signup-submit">signup</button>
                 <p class="login-status">You are logged out</p>
